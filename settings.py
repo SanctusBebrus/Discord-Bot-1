f4be814085e7cb81463f22e1f7be6a274d4e5bb6
Nikita <117333817+SanctusBebrus@users.noreply.github.com>
2023-04-15 20:52:17 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ yandex_api_settings = {
 discord_settings = {
     'token': 'MTA5NjM4ODExNjM5NjM4NDI1Ng.GwC1cy.cflOxyomLW_-bRPrb1Lljt4fGXy5kZpR8dI2Q0',
     'command_prefix': '!',
-    'ffmpeg_path': 'C://ffmpeg//bin//ffmpeg.exe'
+    'ffmpeg_path': 'C://ffmpeg//bin//ffmpeg.exe',
+    'bot_icon': 'https://avatars.mds.yandex.net/i?id=f52622ec6beb8e5e66d0818a0cb78d069e7945d2-5294444-images-thumbs&ref=rim&n=33&w=188&h=188'
 }
