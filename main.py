17ca46ece1ecd2fd16a2f271abd9b2aac369df7b
SanctusBebrus <nikita01252005@gmail.com>
2023-04-15 20:47:47 +1000

    Logging and some cosmetic features

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import discord
 from discord import FFmpegPCMAudio
 import requests
+import logging
 from discord.ext import commands
 from settings import discord_settings
 from yandex_music_api import get_tracks_info, download_track
 import random
+
+logging.basicConfig(
+    filename='events.log',
+    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+)
 
 bot = commands.Bot(command_prefix=discord_settings['command_prefix'],
                    intents=discord.Intents.all())
@@ -28,14 +34,16 @@ async def hello(ctx):  # Поприветствовать пользовател
     author = ctx.message.author
     await ctx.send(f'Привет, {author.mention}!')
 
+
 @bot.command()
 async def randomimage(ctx: discord.ext.commands.context.Context):
     print(type(ctx))
     response = requests.get(f'https://picsum.photos/1024')
     embed = discord.Embed(color=0xff9900, title='Random Image')
-    embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar.url)
+    embed.set_footer(text='Yandex Music Bot', icon_url=discord_settings['bot_icon'])
     embed.set_image(url=response.url)
-    await ctx.send(embed=embed)
+    await ctx.send(ctx.message.author.mention, embed=embed)
+
 
 @bot.command()
 async def play(ctx, *, name_of_song):
@@ -43,14 +51,17 @@ async def play(ctx, *, name_of_song):
     await ctx.send('***Ищу вашу песенку в Яндекс Музыке...***')
 
     if not tracks_info:
-        await ctx.send('К сожалению, я не ничего не нашёл')
+        await ctx.send(f'{ctx.message.author.mention} К сожалению, я не ничего не нашёл')
         return
 
-    to_say = '**Вот что я нашёл!**\n'
+    to_say = ''
+    embed = discord.Embed(color=0xff9900, title='**Вот что я нашёл!**')
     for i, track in enumerate(tracks_info, 1):
         to_chose.append(track)
-        to_say += f'**{i}:** {track["title"]} - {", ".join(track["artists"])} `Длительность: {track["duration"]}`\n'
-    await ctx.send(to_say)
+        to_say += f'**{i}:** {track["title"]} - {", ".join(track["artists"])}\t`Длительность: {track["duration"]}`\n'
+    embed.add_field(name="", value=f"{to_say}", inline=False)
+    embed.set_footer(text='Yandex Music Bot', icon_url=discord_settings['bot_icon'])
+    await ctx.send(ctx.message.author.mention, embed=embed)
 
 
 @bot.command()
@@ -102,7 +113,7 @@ async def c(ctx, num: int):
 async def leave(ctx):  # Ливнуть из войса
     if ctx.voice_client:
         await ctx.guild.voice_client.disconnect()
-        await ctx.send('Пока-пока!')
+        await ctx.send(f'{ctx.message.author.mention} Пока-пока!')
     else:
         await ctx.send('Я не подключен ни к одному из голосовых каналов')
 
