20f1dcc9af56cdd67ecbfe09dabbc24da1f44ec7
Владимир Верхов <103727546+verhovv@users.noreply.github.com>
2023-04-19 22:00:55 +1000

    ALMOST DE FINALE
    -

--- settings.py
+++ settings.py
@@ -1,13 +1,14 @@
 yandex_api_settings = {
-    'token': 'y0_AgAAAAAi2a0qAAG8XgAAAADa5DtNRNdICybNQqyWX7wnkzi7m-VV28Y'
+    'token': 'token'
 }
 
 discord_settings = {
-    'token': 'токен',
+    'token': 'token',
     'command_prefix': 'ya!',
     'ffmpeg_path': 'C://ffmpeg//bin//ffmpeg.exe',
     'embed_footer': {
         'icon_url': 'https://music.yandex.com/blocks/meta/i/og-image.png',
         'text': 'Yandex Music Bot'
-    }
+    },
+    'loading_gif_url': 'https://media.tenor.com/YayvrhznSVwAAAAC/yandex-yandexru.gif'
 }
